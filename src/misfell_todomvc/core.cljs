7e79e9a804655a4a62103d6e6ef0cd46db4ebead
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-03-13 19:11:58 +0200

    Enable done/todo toggling. Add ui-state and transient editing property.

--- src/misfell_todomvc/core.cljs
+++ src/misfell_todomvc/core.cljs
@@ -1,17 +1,23 @@
 (ns misfell-todomvc.core
   (:require [mistletoe.signal :as signal :refer [smap]]
             [mistletoe.signal.util :refer [seqsig->sigseq map-key-cached]]
-            [mistletoe.dom :refer [el]]))
+            [mistletoe.dom :refer [el append-child!]]))
 
 ;;;;
 
-(def ^:private state
-  (signal/source {:todos   {0 {:id    0
-                               :title "Taste JavaScript"
-                               :done  true}
-                            1 {:id    1
-                               :title "Buy a unicorn"
-                               :done  false}}
+(def ^:private domain-state
+  (signal/source {:todos {0 {:id    0
+                             :title "Taste JavaScript"
+                             :done  true}
+                          1 {:id    1
+                             :title "Buy a unicorn"
+                             :done  false}}}))
+
+(def ^:private ui-state
+  (signal/source {:todos   (into {}
+                                 (map (fn [[id _]] [id {:id      id
+                                                        :editing false}]))
+                                 (:todos @domain-state))
                   :filters [{:title    "All"
                              :route    "#/"
                              :selected true}
@@ -29,10 +35,15 @@
       (el :h1 "todos")
       (el :input :class "new-todo" :placeholder "What needs to be done?" :autofocus true)))
 
+(defn- todo-view-class [{:keys [done editing]}]
+  (str (when done "completed ") (when editing "editing")))
+
 (defn- todo-view [todo]
-  (el :li :class (smap #(if (:done %) "completed" "") todo)
+  (el :li :class (smap todo-view-class todo)
       (el :div :class "view"
-          (el :input :class "toggle" :type "checkbox" :checked (smap #(or (:done %) nil) todo))
+          (el :input :class "toggle" :type "checkbox"
+              :checked (smap #(or (:done %) nil) todo)
+              :onchange #(swap! domain-state update-in [:todos (:id @todo) :done] not)) ; HACK
           (el :label (smap :title todo))
           (el :button :class "destroy"))
 
@@ -58,14 +69,15 @@
       (filters-view filters)
       (el :button :class "clear-completed" "Clear completed")))
 
-(defn- todos-view [todos]
+(defn- todos-view [todos ui-todos]
   (el :section :class "main"
       (el :input :id "toggle-all" :class "toggle-all" :type "checkbox")
       (el :label :for "toggle-all" "Mark all as complete")
       (el :ul :class "todo-list"
           (let [todos-sigsig (smap (fn [todos-val]
                                      (for [[id _] todos-val]
-                                       (smap #(get % id) todos)))
+                                       (smap (fn [todos ui-todos] (merge (get todos id) (get ui-todos id)))
+                                             todos ui-todos)))
                                    todos)]
             (smap (map-key-cached (fn [_ todo] (:id @todo))
                                   todo-view)
@@ -76,7 +88,8 @@
 (defn main
   "I don't do a whole lot ... yet."
   []
+  ;; GOTCHA: Have to use append-child! instead of .appendChild or reactivity does not manifest:
   (doto (aget (.getElementsByClassName js/document "todoapp") 0)
-    (.appendChild (header))
-    (.appendChild (todos-view (smap :todos state)))
-    (.appendChild (footer (smap :todos state) (smap :filters state)))))
+    (append-child! (header))
+    (append-child! (todos-view (smap :todos domain-state) (smap :todos ui-state)))
+    (append-child! (footer (smap :todos domain-state) (smap :filters ui-state)))))
